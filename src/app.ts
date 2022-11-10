061da0840a6020160d60b77a84a1882a0b217e4c
antonio <ruggieroantonio1703@gmail.com>
2022-11-10 10:26:24 +0100

    esercizio-2

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import express from "express";
 import "express-async-errors";
 
 import prisma from "./lib/prisma/client";
+import {
+    validate,
+    validationErrorMiddleware,
+    planetSchema,
+    PlanetData,
+} from "./lib/validation";
 
 const app = express();
 
@@ -13,11 +19,21 @@ app.get("/planets", async (request, response) => {
     response.json(planets);
 });
 
-app.post("/planets", async (request, response) => {
-    const planet = request.body;
+app.post(
+    "/planets",
+    validate({ body: planetSchema }),
+    async (request, response) => {
+        const planetData: PlanetData = request.body;
 
-    response.status(201).json(planet);
-});
+        const planet = await prisma.planet.create({
+            data: planetData,
+        });
+
+        response.status(201).json(planet);
+    }
+);
+
+app.use(validationErrorMiddleware);
 
 export default app;
 
