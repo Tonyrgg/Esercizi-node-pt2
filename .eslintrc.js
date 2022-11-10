e12d6bc07bb4d6050c7b70ef706e4ffda30753b8
antonio <ruggieroantonio1703@gmail.com>
2022-11-10 10:31:56 +0100

    esercizio-7

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,52 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  overrides: [
+    {
+      files: [
+        "*.ts"
+      ],
+      rules: {
+        "no-undef": [
+          "off",
+          {
+            typeof: false
+          }
+        ]
+      }
     },
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
+    {
+      files: [
+        "*.test.ts"
+      ],
+      rules: {},
+      env: {
+        jest: true,
+        node: true
+      }
+    }
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: [
+    "@typescript-eslint"
+  ],
+  rules: {
+    "@typescript-eslint/ban-ts-comment": [
+      "off"
     ],
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
-    overrides: [
-        {
-            files: ["*.ts"],
-            rules: {
-                "no-undef": "off",
-            },
-        },
-        {
-            files: ["*.test.ts"],
-            env: {
-                node: true,
-                jest: true,
-            },
-        },
-    ],
-    rules: {
-        "@typescript-eslint/ban-ts-comment": "off",
-    },
+    "@typescript-eslint/no-var-requires": [
+      "off"
+    ]
+  }
 };
